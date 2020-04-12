113b30dc40b914490cd5f62267924060574f681e
Damien Gallagher <damiengallagher@Damiens-MacBook-Pro-16.local>
2020-04-12 12:42:43 +0100

    Update to sender email

--- pkg/handlers/email.go
+++ pkg/handlers/email.go
@@ -13,7 +13,7 @@ import (
 const (
 	// Replace sender@example.com with your "From" address.
 	// This address must be verified with Amazon SES.
-		Sender = "no_reply@myemaol.com"
+		Sender = "no_reply@myemail.com"
 
 	// Replace recipient@example.com with a "To" address. If your account
 	// is still in the sandbox, this address must be verified.
